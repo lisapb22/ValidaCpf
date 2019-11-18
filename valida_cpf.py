9ca0c17cfa8c465de1b5cb30081b4d5e4d6b39eb
BrunoRF <50593937+brunorichartzf@users.noreply.github.com>
2019-11-18 15:56:25 -0300

    Add files via upload

--- valida_cpf.py
+++ valida_cpf.py
@@ -3,11 +3,17 @@ cpf = ("529.982.247-25")
 def valida_cpf(cpf):
     cpf.replace(".","")
     cpf.replace("-","")
-    if len(cpf) > 11 or len(cpf) < 11:
+    tam = len(cpf)
+    if tam > 11 or tam < 11:
         return False
     elif cpf == "00000000000" or cpf == "11111111111" or cpf == "22222222222" or cpf == "33333333333" or cpf == "44444444444" or cpf == "55555555555" or cpf == "66666666666" or cpf == "77777777777" or cpf == "88888888888" or cpf == "99999999999":
         return False
-    elif 
-
-
-#Fonte:https://dicasdeprogramacao.com.br/algoritmo-para-validar-cpf/
+    num_mult = 10
+    soma = 0
+    for i in range(0, tam-3):
+        soma += cpf[i] * num_mult
+        num_mult -= 1
+        i += 1
+    if soma*10//11 == 10:
+        
+    
